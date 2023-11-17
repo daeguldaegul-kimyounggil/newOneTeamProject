47462bd8b73f013cbce5da3237006a2a1aca1741
Halcyon1920 <85556653+hongbumm@users.noreply.github.com>
2023-11-17 13:54:26 +0900

    주성 지피티 성공 111_13:53

--- OneTeamProject/app/src/main/java/com/lx/oneteamproject/main/DialogFragment.kt
+++ OneTeamProject/app/src/main/java/com/lx/oneteamproject/main/DialogFragment.kt
@@ -1,5 +1,6 @@
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,8 @@ import com.lx.oneteamproject.R
 import com.lx.oneteamproject.fragment.FragmentType
 import com.lx.oneteamproject.databinding.FragmentReportCheckPopupBinding
 import com.lx.oneteamproject.sub.SubActivity
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class ReportCheckPopupFragment : DialogFragment() {
 
@@ -20,6 +23,13 @@ class ReportCheckPopupFragment : DialogFragment() {
     ): View? {
         _binding = FragmentReportCheckPopupBinding.inflate(inflater, container, false)
         val view = binding.root
+
+
+        val userInput = readUserInputFromFile()
+
+        binding.reportTitle.text = readResultTitleFromFile()
+        binding.reportTextOutput.text =  readResultCategoryFromFile() + "\n" + userInput
+
 
         // "신고하기" 버튼 클릭 시
         binding.button.setOnClickListener {
@@ -40,6 +50,72 @@ class ReportCheckPopupFragment : DialogFragment() {
         }
 
         return view
+    }
+
+    private fun readUserInputFromFile(): String {
+        var userInput = ""
+        try {
+            val fileName = "com.lx.oneteamproject.chatgpt.gptUserInput"
+            val fileInputStream = requireContext().openFileInput(fileName)
+            val inputStreamReader = InputStreamReader(fileInputStream)
+            val bufferedReader = BufferedReader(inputStreamReader)
+            val stringBuilder = StringBuilder()
+            var text: String? = null
+            while ({ text = bufferedReader.readLine(); text }() != null) {
+                stringBuilder.append(text).append("\n")
+            }
+            fileInputStream.close()
+            userInput = stringBuilder.toString()
+            Log.d("ReadUserInput", "User input read from file: $userInput")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.e("ReadUserInput", "Error reading user input from file: ${e.message}")
+        }
+        return userInput
+    }
+
+    private fun readResultTitleFromFile(): String {
+        var resultTitle = ""
+        try {
+            val fileName = "com.lx.oneteamproject.chatgpt.gptResultTitle"
+            val fileInputStream = requireContext().openFileInput(fileName)
+            val inputStreamReader = InputStreamReader(fileInputStream)
+            val bufferedReader = BufferedReader(inputStreamReader)
+            val stringBuilder = StringBuilder()
+            var text: String? = null
+            while ({ text = bufferedReader.readLine(); text }() != null) {
+                stringBuilder.append(text).append("\n")
+            }
+            fileInputStream.close()
+            resultTitle = stringBuilder.toString()
+            Log.d("ReadUserInput", "User input read from file: $resultTitle")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.e("ReadUserInput", "Error reading user input from file: ${e.message}")
+        }
+        return resultTitle
+    }
+
+    private fun readResultCategoryFromFile(): String {
+        var resultCategory = ""
+        try {
+            val fileName = "com.lx.oneteamproject.chatgpt.gptResultCategory"
+            val fileInputStream = requireContext().openFileInput(fileName)
+            val inputStreamReader = InputStreamReader(fileInputStream)
+            val bufferedReader = BufferedReader(inputStreamReader)
+            val stringBuilder = StringBuilder()
+            var text: String? = null
+            while ({ text = bufferedReader.readLine(); text }() != null) {
+                stringBuilder.append(text).append("\n")
+            }
+            fileInputStream.close()
+            resultCategory = stringBuilder.toString()
+            Log.d("ReadUserInput", "User input read from file: $resultCategory")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.e("ReadUserInput", "Error reading user input from file: ${e.message}")
+        }
+        return resultCategory
     }
 
     override fun onDestroyView() {
--- OneTeamProject/app/src/main/java/com/lx/oneteamproject/main/ReportCheckPopUpFragment.kt
+++ OneTeamProject/app/src/main/java/com/lx/oneteamproject/main/ReportCheckPopUpFragment.kt
@@ -2,12 +2,16 @@ package com.lx.oneteamproject.main
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.lx.oneteamproject.fragment.OnFragmentListener
 import androidx.fragment.app.Fragment
 import com.lx.oneteamproject.databinding.FragmentReportCheckPopupBinding
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class ReportCheckPopUpFragment : Fragment(){
 
@@ -16,8 +20,11 @@ class ReportCheckPopUpFragment : Fragment(){
 
     var listener: OnFragmentListener? = null
 
+
+
     override fun onAttach(activity: Activity) {
         super.onAttach(activity)
+
 
         if (activity is OnFragmentListener) {
             listener = activity as OnFragmentListener
@@ -33,7 +40,83 @@ class ReportCheckPopUpFragment : Fragment(){
 
         binding.reportImageOutput.adapter = ReportFreeImagePagerAdapter(requireActivity().supportFragmentManager, lifecycle)
 
+
+        /*
+
+        binding.reportTitle.hint="sdfsdfsdf"
+        val userInput = readUserInputFromFile()
+
+        binding.reportTitle.text = "sdfsdf"
+        binding.reportTextOutput.text = "rerereR"
+        */
+
         return binding.root
+    }
+
+    private fun readUserInputFromFile(): String {
+        var userInput = ""
+        try {
+            val fileName = "com.lx.oneteamproject.chatgpt.gptUserInput"
+            val fileInputStream = requireContext().openFileInput(fileName)
+            val inputStreamReader = InputStreamReader(fileInputStream)
+            val bufferedReader = BufferedReader(inputStreamReader)
+            val stringBuilder = StringBuilder()
+            var text: String? = null
+            while ({ text = bufferedReader.readLine(); text }() != null) {
+                stringBuilder.append(text).append("\n")
+            }
+            fileInputStream.close()
+            userInput = stringBuilder.toString()
+            Log.d("ReadUserInput", "User input read from file: $userInput")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.e("ReadUserInput", "Error reading user input from file: ${e.message}")
+        }
+        return userInput
+    }
+
+    private fun readResultTitleFromFile(): String {
+        var resultTitle = ""
+        try {
+            val fileName = "com.lx.oneteamproject.chatgpt.gptResultTitle"
+            val fileInputStream = requireContext().openFileInput(fileName)
+            val inputStreamReader = InputStreamReader(fileInputStream)
+            val bufferedReader = BufferedReader(inputStreamReader)
+            val stringBuilder = StringBuilder()
+            var text: String? = null
+            while ({ text = bufferedReader.readLine(); text }() != null) {
+                stringBuilder.append(text).append("\n")
+            }
+            fileInputStream.close()
+            resultTitle = stringBuilder.toString()
+            Log.d("ReadUserInput", "User input read from file: $resultTitle")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.e("ReadUserInput", "Error reading user input from file: ${e.message}")
+        }
+        return resultTitle
+    }
+
+    private fun readResultCategoryFromFile(): String {
+        var resultCategory = ""
+        try {
+            val fileName = "com.lx.oneteamproject.chatgpt.gptResultCategory"
+            val fileInputStream = requireContext().openFileInput(fileName)
+            val inputStreamReader = InputStreamReader(fileInputStream)
+            val bufferedReader = BufferedReader(inputStreamReader)
+            val stringBuilder = StringBuilder()
+            var text: String? = null
+            while ({ text = bufferedReader.readLine(); text }() != null) {
+                stringBuilder.append(text).append("\n")
+            }
+            fileInputStream.close()
+            resultCategory = stringBuilder.toString()
+            Log.d("ReadUserInput", "User input read from file: $resultCategory")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.e("ReadUserInput", "Error reading user input from file: ${e.message}")
+        }
+        return resultCategory
     }
 
     override fun onDestroyView() {
@@ -46,4 +129,6 @@ class ReportCheckPopUpFragment : Fragment(){
 
         listener = null
     }
+
+
 }
